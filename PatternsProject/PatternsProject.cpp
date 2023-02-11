37d532a126ae8298c9fe144c9cd249f84fbd8ccd
admin <admin@KGID6>
2023-02-11 09:46:46 +0300

    Добавьте файлы проекта.

---
+++ PatternsProject/PatternsProject.cpp
@@ -0,0 +1,91 @@
+﻿#include <iostream>
+#include <map>
+
+using namespace std;
+
+//IExpression ::= NumberExpression | Constant | AddExpr | SubstractExpr
+//
+//AddExpr ::= IExpression + IExpression
+//SubstractExpr :: = IExpression - IExpression
+//
+//NumberExpression ::= [A-Z,a-z]+
+//Constant ::= [0-9]+
+
+class Context
+{
+    map<string, int> variables;
+public:
+    int GetVariable(string key)
+    {
+        return variables[key];
+    }
+
+    void SetVariable(string key, int value)
+    {
+        if (variables.contains(key))
+            variables[key] = value;
+        else
+            variables.insert({ key, value });
+    }
+};
+
+class IExpression
+{
+public:
+    virtual int Iterpret(Context* context) = 0;
+};
+
+class NumberExpression : public IExpression
+{
+    string name;
+public:
+    NumberExpression(string name) : name{ name } {}
+    int Iterpret(Context* context) override
+    {
+        return context->GetVariable(name);
+    }
+};
+
+class AddExpression : public IExpression
+{
+    IExpression* expressionLeft;
+    IExpression* expressionRight;
+public:
+    AddExpression(IExpression* expressionLeft, IExpression* expressionRight)
+        : expressionLeft{ expressionLeft }, expressionRight{ expressionRight } {}
+    int Iterpret(Context* context) override
+    {
+        return expressionLeft->Iterpret(context) + expressionRight->Iterpret(context);
+    }
+};
+
+class SubExpression : public IExpression
+{
+    IExpression* expressionLeft;
+    IExpression* expressionRight;
+public:
+    SubExpression(IExpression* expressionLeft, IExpression* expressionRight)
+        : expressionLeft{ expressionLeft }, expressionRight{ expressionRight } {}
+    int Iterpret(Context* context) override
+    {
+        return expressionLeft->Iterpret(context) - expressionRight->Iterpret(context);
+    }
+};
+
+int main()
+{
+    Context* context = new Context();
+    context->SetVariable("a", 50);
+    context->SetVariable("b", 20);
+    context->SetVariable("c", 30);
+
+    IExpression* expression = new SubExpression(
+        new AddExpression(new NumberExpression("a"), new NumberExpression("b")),
+        new NumberExpression("c")
+    );
+
+    int result = expression->Iterpret(context);
+
+    cout << result;
+}
+
